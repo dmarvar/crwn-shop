b7307ddc8ff35121ac17b7add26f42bff72490d7
Arturo Martinez <d.martinez@sisproconsultores.com>
2020-04-05 12:55:49 -0500

    Corrected selection of is loading for spinner in collection page

--- src/pages/collection/collection.container.jsx
+++ src/pages/collection/collection.container.jsx
@@ -3,10 +3,10 @@ import { compose } from "redux";
 import { createStructuredSelector } from "reselect";
 import WithSpinner from "../../components/with-spinner/with-spinner.component";
 import CollectionPage from "./collection.component";
-import { selectIsCollectionFetching } from "../../redux/shop/shop.selectors";
+import { selectIsCollectionLoaded } from "../../redux/shop/shop.selectors";
 
 const mapStateToProps = createStructuredSelector({
-  isLoading: selectIsCollectionFetching
+  isLoading: state => !selectIsCollectionLoaded(state)
 });
 
 const CollectionPageContainer = compose(
